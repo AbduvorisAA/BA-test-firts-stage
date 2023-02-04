bbfaeb736634aff3340d836acb5eb13138da267e
Abduvoris <abduvorisabduvakilov0@gmail.com>
2023-02-04 19:52:49 +0500

    updated

---
+++ main.py
@@ -0,0 +1,24 @@
+# Task-1
+# days = int(input("Enter days: "))
+# year = days // 365
+# week = (days - year * 365) // 7
+# day = days - year * 365 - week * 7
+# print(year, week, day)
+
+# Task-2
+# _string = input("hi hello mello yello tello")
+# sub_string = input("hi")
+# if sub_string in _string:
+#     print(True)
+# else:
+#     print(False)
+
+# Task-3
+# _string = input()
+# lst = "abcdefghijklmnopqrstuvwxyz"
+# for i in lst:
+#     if i not in _string.lower():
+#         print(False)
+#         quit()
+#
+# print(True)
